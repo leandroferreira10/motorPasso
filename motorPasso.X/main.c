e399bdfeb36bd13cb82ab1c5edcad7051e0b4f8d
10 <10@DESKTOP-LEHK6IM>
2021-06-03 10:49:24 -0300

    Função principal main.

--- motorPasso.X/main.c
+++ motorPasso.X/main.c
@@ -11,14 +11,18 @@
 #include "stepMotor.h"
 #include "delay.h"
 
-void main(void) 
-{
-    stepMotor_init (8);
-    while ( 1 )
-    {
-        stepMotor ( MEIOPASSO, SH, 360, 1000);
-        delay(3000);
-       // stepMotor ( CCW, 360, 1000);
-       // delay(3000);
-    }
-}
+void main(void)                                                // Função principal main.
+{                                                              // Inicio da função Main.
+    stepMotor_init (32);                                       // Configuração de inicialização.
+    while ( 1 )                                                // Laço de repetição infinito.
+    {                                                          // Inicio do laço de repetição
+        stepMotor ( PASSO, SENTIDO_HORARIO, 90, 500);          // Passos, sentido, graus e tempo.
+        delay(3000);                                           // Espera de 3000 ms = 3s.
+        stepMotor ( MEIO_PASSO, SENTIDO_HORARIO , 270, 500);   // Passos, sentido, graus, tempo.
+        delay(3000);                                           // Espera de 3000 ms = 3s.
+        stepMotor ( PASSO, ANTI_HORARIO , 90, 500);            // Passos, sentido, graus, tempo.
+        delay(3000);                                           // Espera de 3000 ms = 3s.
+        stepMotor ( MEIO_PASSO, ANTI_HORARIO , 270, 500);      // Passos, sentido, graus, tempo.
+        delay(3000);                                           // Espera de 3000 ms = 3s.
+    }                                                          // fim do laço de repetição.
+}                                                              // fim da função main.
